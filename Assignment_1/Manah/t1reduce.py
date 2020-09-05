a9e72b57c62baa5d6a213d5842fc6b1fbdf3227e
Manah Shetty <56164920+manahshetty@users.noreply.github.com>
2020-09-05 20:04:56 +0530

    Create t1reduce.py

---
+++ Assignment_1/Manah/t1reduce.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#Task 1 Reducer
+
+import sys
+
+cur_rec = None
+cur_count = 0
+rec = None
+
+for line in sys.stdin:
+    line = line.strip()
+    try:
+    	rec,count = line.split("\t",1)
+    except ValueError:
+    	print(line)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if cur_rec == rec:
+        cur_count += count
+    else:
+        if cur_rec:
+            print(cur_count)
+        cur_count = count
+        cur_rec = rec
+
+if cur_rec==rec:
+    print(cur_count)
