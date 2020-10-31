e18ebb89136e4a748321469fb2b9560ae8e95cb3
Saurav51 <51379789+Saurav51@users.noreply.github.com>
2020-10-31 22:01:09 +0530

    Format output & column names

--- Assignment_3/Task1_1.py
+++ Assignment_3/Task1_1.py
@@ -9,28 +9,20 @@ pathDataset1 = sys.argv[2]
 pathDataset2 = sys.argv[3]
 
 spark = SparkSession.builder.master("local").appName("A3T1").config(conf=SparkConf()).getOrCreate()
-# sc = SparkContext(master='local',appName='A3T1')
 
 shape = spark.read.csv(pathDataset1)
-shapeStat = spark.read.csv(pathDataset2)
+shapeStat = spark.read.option('header',True).csv(pathDataset2)
 
-wordmatch = shapeStat.filter(shapeStat['_c0']==searchWord)
+wordmatch = shapeStat.filter(shapeStat['word']==searchWord)
 
-avgStrokes = wordmatch.groupBy('_c2').agg({"_c4":'avg'}).collect()
+avgStrokes = wordmatch.groupBy('recognized').agg({"Total_Strokes":'avg'}).collect()
 
 for i in avgStrokes:
-    print(i[1]) 
-    #print(i.Total_Strokes)
-
-# Output for 'alarm clock'
-# 11.916666...
-# 8.26737...
-
+    print("%.5f" %i[1]) 
+    
 '''
-Changes from Task-1.py
-1. Column names had to be referenced as (_c0,_c1,...) and not the actual column names. Not sure if this is just in my system.
-2. print(i[1]) - this worked for me
-3.  Output for word 'alarm clock' : 
-     11.916666...
-     8.26737.....
+Output for 'alarm clock'
+11.91667
+8.26738
 '''
+    
