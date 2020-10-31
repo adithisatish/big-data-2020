d3dbd57ec72196c7dcc6c177f2d41c856decc8ae
Saurav51 <51379789+Saurav51@users.noreply.github.com>
2020-10-31 11:03:43 +0530

    Add files via upload
    1. Column names had to be referenced as (_c0,_c1,...) and not the actual column names. Not sure if this is just in my system.
    2. print(i[1]) - this worked for me
    3.  Output for word 'alarm clock' : 
         11.916666...
         8.26737.....

---
+++ Assignment_3/Task1_1.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+
+import sys
+from pyspark import SparkContext,SparkConf
+from pyspark.sql import SparkSession
+
+searchWord = sys.argv[1]
+pathDataset1 = sys.argv[2]
+pathDataset2 = sys.argv[3]
+
+spark = SparkSession.builder.master("local").appName("A3T1").config(conf=SparkConf()).getOrCreate()
+# sc = SparkContext(master='local',appName='A3T1')
+
+shape = spark.read.csv(pathDataset1)
+shapeStat = spark.read.csv(pathDataset2)
+
+wordmatch = shapeStat.filter(shapeStat['_c0']==searchWord)
+
+avgStrokes = wordmatch.groupBy('_c2').agg({"_c4":'avg'}).collect()
+
+for i in avgStrokes:
+    print(i[1]) 
+    #print(i.Total_Strokes)
