3f263a0d699fa67b19fcea8a7833d70c67840fc8
Saurav51 <51379789+Saurav51@users.noreply.github.com>
2020-11-01 13:24:03 +0530

    Update task1.py

--- Assignment_3/task1.py
+++ Assignment_3/task1.py
@@ -17,23 +17,23 @@ shapeStat = spark.read.option('header',True).csv(pathDataset2)
 wordmatch = shapeStat.filter(shapeStat['word']==searchWord)
 
 # Checking if the word was not found and appropriately printing zeroes
-if wordmatch.collect() == None:
-    print(0.00000,0.00000,sep='\n')
+if len(wordmatch.collect()) == 0:
+    print("%.5f" %0.00000,"%.5f" %0.00000,sep='\n')
     
 else:
     avgStrokes = wordmatch.groupBy('recognized').agg({"Total_Strokes":'avg'}).orderBy('recognized',ascending=False).collect()
 
-    if avgStrokes == None: # Neither recognized nor unrecognized found - impossible case as specs say clean dataset will be provided, but precautionary measure
-        print(0.00000,0.00000,sep='\n')
+    if (len(avgStrokes)) == 0: # Neither recognized nor unrecognized found - impossible case as specs say clean dataset will be provided, but precautionary measure
+        print("%.5f" %0.00000,"%.5f" %0.00000,sep='\n')
     
     else:
         if len(avgStrokes)==1: # Either recognized or unrecognized found
 
             if avgStrokes[0][0] == True: # No unrecognized found - print zero, check if True works or it's 'TRUE' i.e all caps and string
                 print("%.5f"%avgStrokes[0][1])
-                print(0.00000)
+                print("%.5f" %0.00000)
             else: # No recognized found
-                print(0.00000)
+                print("%.5f" %0.00000)
                 print("%.5f"%avgStrokes[0][0])
                 
         else: # Both recognized and unrecognized found
@@ -42,6 +42,10 @@ else:
     
 '''
 Output for 'alarm clock'
-11.91667
 8.26738
+11.91667
+
+Output for 'random word'
+0.00000
+0.00000
 '''    
